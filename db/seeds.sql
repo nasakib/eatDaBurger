8dbbe9322e5499c288999a3e0e2cac488a206a59
nasakib <nat.sakib@gmail.com>
2020-04-10 22:54:51 -0400

    Mostly changed

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Mushroom Burger');
+INSERT INTO burgers (name) VALUES ('Cheese Burger');
+INSERT INTO burgers (name, sleepy) VALUES ('Double Burger', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Quarter Pound', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Joe Biden', true);
+INSERT INTO burgers (name) VALUES ('Impossible Burger');
